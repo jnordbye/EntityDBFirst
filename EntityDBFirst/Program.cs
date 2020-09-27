c5f13d367d8f223f569424d7114c7675b473016d
jnordbye <jnordbye@outlook.com>
2020-09-26 19:36:21 -0700

    Database Changes made

--- EntityDBFirst/Program.cs
+++ EntityDBFirst/Program.cs
@@ -11,6 +11,16 @@ namespace EntityDBFirst
         static void Main(string[] args)
         {
             // test of database first in Entity framework
+            var dbContext = new PlutoDBContext();
+            var courses = dbContext.GetCourses();
+
+            foreach(var c in courses)
+            {
+                Console.WriteLine(c.Title );
+            }
+
+            Console.WriteLine("\n\tPress enter to exit");
+            Console.ReadLine();   // pause before exit
         }
     }
 }
